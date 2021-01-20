d176df60469df107b1770bff0f97e623ba5eb8f7
bilisimmedresesi <72303537+bilisimmedresesi@users.noreply.github.com>
2021-01-20 22:05:32 +0300

    Add files via upload

---
+++ GunBulma/GunBulma/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace GunBulma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtTarih = new System.Windows.Forms.DateTimePicker();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtTarih
+            // 
+            this.dtTarih.Location = new System.Drawing.Point(12, 18);
+            this.dtTarih.Name = "dtTarih";
+            this.dtTarih.Size = new System.Drawing.Size(247, 20);
+            this.dtTarih.TabIndex = 0;
+            this.dtTarih.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dateTimePicker1_KeyDown);
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(17, 55);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(35, 13);
+            this.lbResult.TabIndex = 1;
+            this.lbResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 100);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.dtTarih);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtTarih;
+        private System.Windows.Forms.Label lbResult;
+    }
+}
+
---
+++ GunBulma/GunBulma/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GunBulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            //20 haziran 2021
+            if (e.KeyCode == Keys.Return)
+            {
+                DateTime dt = dtTarih.Value;
+                int[] dayOfMonth = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                double gecenGun = ((dt.Year - 1) * 365 + (dt.Year / 4) - (dt.Year / 100 * 0.75)); // 01.01.0001 den  01.01.2021   
+
+                for (int i = 0; i < dt.Month - 1; i++) // haziran 1 e kadar olan günü toplayacağım    6.ay
+                {
+                    gecenGun += dayOfMonth[i];
+
+                    if (i == 1 && dt.Year % 4 == 0 && (dt.Year % 400 == 0 || dt.Year % 100 > 0))
+
+                        gecenGun++; // toplandı
+
+                }
+                double toplam = (gecenGun + dt.Day - 1) - (dt.Year % 4 == 0 ? 1 : 0) + (dt.Year % 100 == 0 ? 1 : 0) - (dt.Year % 400 == 0 ? 1 : 0);
+                int calc = (Convert.ToInt32(Math.Floor(toplam)) + 1) % 7;
+                string gun = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetDayName((DayOfWeek)calc);
+
+                lbResult.Text = toplam.ToString() + "   /  " + gun;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
