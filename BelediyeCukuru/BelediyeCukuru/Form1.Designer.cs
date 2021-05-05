9cae499399d62ca5e47082604629d7df35ec71e7
bilisimmedresesi <bilisimmedresesi@hotmail.com>
2021-05-06 01:04:08 +0300

    Belediye Cukuru Oyunu
    Belediye Çukuru Oyunu

---
+++ BelediyeCukuru/BelediyeCukuru/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace BelediyeCukuru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLevel11 = new System.Windows.Forms.Button();
+            this.btnLevel12 = new System.Windows.Forms.Button();
+            this.btnLevel13 = new System.Windows.Forms.Button();
+            this.btnLevel23 = new System.Windows.Forms.Button();
+            this.btnLevel22 = new System.Windows.Forms.Button();
+            this.btnLevel21 = new System.Windows.Forms.Button();
+            this.btnLevel33 = new System.Windows.Forms.Button();
+            this.btnLevel32 = new System.Windows.Forms.Button();
+            this.btnLevel31 = new System.Windows.Forms.Button();
+            this.btnLevel43 = new System.Windows.Forms.Button();
+            this.btnLevel42 = new System.Windows.Forms.Button();
+            this.btnLevel41 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLevel11
+            // 
+            this.btnLevel11.BackColor = System.Drawing.Color.Navy;
+            this.btnLevel11.Location = new System.Drawing.Point(67, 10);
+            this.btnLevel11.Name = "btnLevel11";
+            this.btnLevel11.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel11.TabIndex = 0;
+            this.btnLevel11.UseVisualStyleBackColor = false;
+            this.btnLevel11.Click += new System.EventHandler(this.btnLevel11_Click);
+            // 
+            // btnLevel12
+            // 
+            this.btnLevel12.BackColor = System.Drawing.Color.Navy;
+            this.btnLevel12.Location = new System.Drawing.Point(127, 10);
+            this.btnLevel12.Name = "btnLevel12";
+            this.btnLevel12.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel12.TabIndex = 1;
+            this.btnLevel12.UseVisualStyleBackColor = false;
+            this.btnLevel12.Click += new System.EventHandler(this.btnLevel12_Click);
+            // 
+            // btnLevel13
+            // 
+            this.btnLevel13.BackColor = System.Drawing.Color.Navy;
+            this.btnLevel13.Location = new System.Drawing.Point(187, 10);
+            this.btnLevel13.Name = "btnLevel13";
+            this.btnLevel13.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel13.TabIndex = 2;
+            this.btnLevel13.UseVisualStyleBackColor = false;
+            this.btnLevel13.Click += new System.EventHandler(this.btnLevel13_Click);
+            // 
+            // btnLevel23
+            // 
+            this.btnLevel23.BackColor = System.Drawing.Color.Orange;
+            this.btnLevel23.Location = new System.Drawing.Point(187, 66);
+            this.btnLevel23.Name = "btnLevel23";
+            this.btnLevel23.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel23.TabIndex = 5;
+            this.btnLevel23.UseVisualStyleBackColor = false;
+            this.btnLevel23.Click += new System.EventHandler(this.btnLevel23_Click);
+            // 
+            // btnLevel22
+            // 
+            this.btnLevel22.BackColor = System.Drawing.Color.Orange;
+            this.btnLevel22.Location = new System.Drawing.Point(127, 66);
+            this.btnLevel22.Name = "btnLevel22";
+            this.btnLevel22.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel22.TabIndex = 4;
+            this.btnLevel22.UseVisualStyleBackColor = false;
+            this.btnLevel22.Click += new System.EventHandler(this.btnLevel22_Click);
+            // 
+            // btnLevel21
+            // 
+            this.btnLevel21.BackColor = System.Drawing.Color.Orange;
+            this.btnLevel21.Location = new System.Drawing.Point(67, 66);
+            this.btnLevel21.Name = "btnLevel21";
+            this.btnLevel21.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel21.TabIndex = 3;
+            this.btnLevel21.UseVisualStyleBackColor = false;
+            this.btnLevel21.Click += new System.EventHandler(this.btnLevel21_Click);
+            // 
+            // btnLevel33
+            // 
+            this.btnLevel33.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.btnLevel33.Location = new System.Drawing.Point(187, 122);
+            this.btnLevel33.Name = "btnLevel33";
+            this.btnLevel33.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel33.TabIndex = 8;
+            this.btnLevel33.UseVisualStyleBackColor = false;
+            this.btnLevel33.Click += new System.EventHandler(this.btnLevel33_Click);
+            // 
+            // btnLevel32
+            // 
+            this.btnLevel32.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.btnLevel32.Location = new System.Drawing.Point(127, 122);
+            this.btnLevel32.Name = "btnLevel32";
+            this.btnLevel32.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel32.TabIndex = 7;
+            this.btnLevel32.UseVisualStyleBackColor = false;
+            this.btnLevel32.Click += new System.EventHandler(this.btnLevel32_Click);
+            // 
+            // btnLevel31
+            // 
+            this.btnLevel31.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.btnLevel31.Location = new System.Drawing.Point(67, 122);
+            this.btnLevel31.Name = "btnLevel31";
+            this.btnLevel31.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel31.TabIndex = 6;
+            this.btnLevel31.UseVisualStyleBackColor = false;
+            this.btnLevel31.Click += new System.EventHandler(this.btnLevel31_Click);
+            // 
+            // btnLevel43
+            // 
+            this.btnLevel43.BackColor = System.Drawing.Color.LightGreen;
+            this.btnLevel43.Location = new System.Drawing.Point(187, 178);
+            this.btnLevel43.Name = "btnLevel43";
+            this.btnLevel43.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel43.TabIndex = 11;
+            this.btnLevel43.UseVisualStyleBackColor = false;
+            this.btnLevel43.Click += new System.EventHandler(this.btnLevel43_Click);
+            // 
+            // btnLevel42
+            // 
+            this.btnLevel42.BackColor = System.Drawing.Color.LightGreen;
+            this.btnLevel42.Location = new System.Drawing.Point(127, 178);
+            this.btnLevel42.Name = "btnLevel42";
+            this.btnLevel42.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel42.TabIndex = 10;
+            this.btnLevel42.UseVisualStyleBackColor = false;
+            this.btnLevel42.Click += new System.EventHandler(this.btnLevel42_Click);
+            // 
+            // btnLevel41
+            // 
+            this.btnLevel41.BackColor = System.Drawing.Color.LightGreen;
+            this.btnLevel41.Location = new System.Drawing.Point(67, 178);
+            this.btnLevel41.Name = "btnLevel41";
+            this.btnLevel41.Size = new System.Drawing.Size(50, 50);
+            this.btnLevel41.TabIndex = 9;
+            this.btnLevel41.UseVisualStyleBackColor = false;
+            this.btnLevel41.Click += new System.EventHandler(this.btnLevel41_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "1.Aşama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "2.Aşama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "3.Aşama";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "4.Aşama";
+            // 
+            // lbResult
+            // 
+            this.lbResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbResult.Location = new System.Drawing.Point(6, 255);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(234, 30);
+            this.lbResult.TabIndex = 16;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lbResult);
+            this.panel1.Controls.Add(this.btnLevel11);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnLevel12);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnLevel13);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnLevel21);
+            this.panel1.Controls.Add(this.btnLevel22);
+            this.panel1.Controls.Add(this.btnLevel43);
+            this.panel1.Controls.Add(this.btnLevel23);
+            this.panel1.Controls.Add(this.btnLevel42);
+            this.panel1.Controls.Add(this.btnLevel31);
+            this.panel1.Controls.Add(this.btnLevel41);
+            this.panel1.Controls.Add(this.btnLevel32);
+            this.panel1.Controls.Add(this.btnLevel33);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(12, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(248, 293);
+            this.panel1.TabIndex = 17;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(93, 23);
+            this.btnStart.TabIndex = 18;
+            this.btnStart.Text = "Yeni Oyun";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 347);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Belediye Çukuru ";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLevel11;
+        private System.Windows.Forms.Button btnLevel12;
+        private System.Windows.Forms.Button btnLevel13;
+        private System.Windows.Forms.Button btnLevel23;
+        private System.Windows.Forms.Button btnLevel22;
+        private System.Windows.Forms.Button btnLevel21;
+        private System.Windows.Forms.Button btnLevel33;
+        private System.Windows.Forms.Button btnLevel32;
+        private System.Windows.Forms.Button btnLevel31;
+        private System.Windows.Forms.Button btnLevel43;
+        private System.Windows.Forms.Button btnLevel42;
+        private System.Windows.Forms.Button btnLevel41;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ BelediyeCukuru/BelediyeCukuru/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BelediyeCukuru
+{
+    public partial class Form1 : Form
+    {
+
+        int[] dolular = new int[4];
+        int deneme = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnLevel11_Click(object sender, EventArgs e)
+        {
+            Check(1, 1);
+        }
+
+        private void btnLevel12_Click(object sender, EventArgs e)
+        {
+            Check(1, 2);
+        }
+
+        private void btnLevel13_Click(object sender, EventArgs e)
+        {
+            Check(1, 3);
+        }
+
+        private void btnLevel21_Click(object sender, EventArgs e)
+        {
+            Check(2, 1);
+        }
+
+        private void btnLevel22_Click(object sender, EventArgs e)
+        {
+            Check(2, 2);
+        }
+
+        private void btnLevel23_Click(object sender, EventArgs e)
+        {
+            Check(2, 3);
+        }
+
+        private void btnLevel31_Click(object sender, EventArgs e)
+        {
+            Check(3, 1);
+        }
+
+        private void btnLevel32_Click(object sender, EventArgs e)
+        {
+            Check(3, 2);
+        }
+
+        private void btnLevel33_Click(object sender, EventArgs e)
+        {
+            Check(3, 3);
+        }
+
+        private void btnLevel41_Click(object sender, EventArgs e)
+        {
+            Check(4, 1);
+        }
+
+        private void btnLevel42_Click(object sender, EventArgs e)
+        {
+            Check(4, 2);
+        }
+
+        private void btnLevel43_Click(object sender, EventArgs e)
+        {
+            Check(4, 3);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ButonlariHazirlaYeniden();
+            deneme = 0;
+            Random random = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                dolular[i] = random.Next(2) + 1;
+            }
+        }
+
+        private void Check(int asama, int tiklananKutuNo)
+        {
+            deneme++;
+            if (asama == 1 && dolular[0] == tiklananKutuNo) // bildi ise
+            {
+                btnLevel11.Enabled = false;
+                btnLevel12.Enabled = false;
+                btnLevel13.Enabled = false;
+                btnLevel21.Enabled = true;
+                btnLevel22.Enabled = true;
+                btnLevel23.Enabled = true;
+                lbResult.Text = "Tebrikler 2.Aşamaya Geçtiniz";
+            }
+            else if (asama == 2 && dolular[1] == tiklananKutuNo)
+            {
+                btnLevel21.Enabled = false;
+                btnLevel22.Enabled = false;
+                btnLevel23.Enabled = false;
+                btnLevel31.Enabled = true;
+                btnLevel32.Enabled = true;
+                btnLevel33.Enabled = true;
+                lbResult.Text = "Tebrikler 3.Aşamaya Geçtiniz";
+            }
+            else if (asama == 3 && dolular[2] == tiklananKutuNo)
+            {
+                btnLevel31.Enabled = false;
+                btnLevel32.Enabled = false;
+                btnLevel33.Enabled = false;
+                btnLevel41.Enabled = true;
+                btnLevel42.Enabled = true;
+                btnLevel43.Enabled = true;
+                lbResult.Text = "Tebrikler 4.Aşamaya Geçtiniz";
+            }
+            else if (asama == 4 && dolular[3] == tiklananKutuNo)
+            {
+                btnLevel41.Enabled = false;
+                btnLevel42.Enabled = false;
+                btnLevel43.Enabled = false;
+                lbResult.Text = "TEBRİKLER KAZANDINIZ . Deneme :" + deneme;
+            }
+            else
+            {
+                lbResult.Text = "Yanlış Tercih..Deneme : " + deneme;
+                ButonlariHazirlaYeniden();
+            }
+        }
+
+        void ButonlariHazirlaYeniden()
+        {
+            panel1.Enabled = true;
+            btnLevel11.Enabled = true;
+            btnLevel12.Enabled = true;
+            btnLevel13.Enabled = true;
+
+            btnLevel21.Enabled = false;
+            btnLevel22.Enabled = false;
+            btnLevel23.Enabled = false;
+
+            btnLevel31.Enabled = false;
+            btnLevel32.Enabled = false;
+            btnLevel33.Enabled = false;
+
+            btnLevel41.Enabled = false;
+            btnLevel42.Enabled = false;
+            btnLevel43.Enabled = false;
+
+        }
+
+    }
+}
